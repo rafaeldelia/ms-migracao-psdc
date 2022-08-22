f8bcac32685489faf96f1a1143b8c4288d44f7f1
finhat2 <finhat2@gmail.com>
2022-08-22 20:01:08 -0300

    subida inicial do projeto

---
+++ src/test/java/br/com/totvs/msmigracaopsdc/MsMigracaoPsdcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.totvs.msmigracaopsdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMigracaoPsdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
